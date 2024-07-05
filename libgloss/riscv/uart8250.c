3525b4bcd9a40e685fe456224d9f7098229aa75d
Andreas Stergiopoulos <andreas.stergiopoulos@tuni.fi>
2024-07-05 14:49:11 +0300

    Restores UART initialization functionality and original driver state

--- libgloss/riscv/uart8250.c
+++ libgloss/riscv/uart8250.c
@@ -53,13 +53,11 @@ static uint32_t uart8250_reg_shift;
 
 uint32_t get_reg(uint32_t num)
 {
-	// return *(uart8250_base);
-	return *((volatile char*)(uart8250_base + num));
-	/* uint32_t offset = num << uart8250_reg_shift;
+	uint32_t offset = num << uart8250_reg_shift;
 
 	if (uart8250_reg_width == 1) return *(uart8250_base + offset);
 	else if (uart8250_reg_width == 2) return *(uart8250_base + offset);
-	else return *(uart8250_base + offset); */
+	else return *(uart8250_base + offset);
 }
 
 void set_reg(
@@ -67,13 +65,11 @@ void set_reg(
     uint32_t val
 )
 {
-	// *(uart8250_base) = val;
-	*((volatile char*)(uart8250_base + num)) = (char)val;
-	/* uint32_t offset = num << uart8250_reg_shift;
+	uint32_t offset = num << uart8250_reg_shift;
 
 	if (uart8250_reg_width == 1) *(uart8250_base + offset) = val;
 	else if (uart8250_reg_width == 2) *(uart8250_base + offset) = val;
-	else *(uart8250_base + offset) = val; */
+	else *(uart8250_base + offset) = val;
 }
 
 void uart8250_putc(char ch)
@@ -88,10 +84,6 @@ char uart8250_getc(void)
 	while ((get_reg(UART_LSR_OFFSET) & UART_LSR_DR) == 0);
 	
 	return get_reg(UART_RBR_OFFSET);
-	
-	// if (get_reg(UART_LSR_OFFSET) & UART_LSR_DR)
-	// 	return get_reg(UART_RBR_OFFSET);
-	// return -1;
 }
 
 /**
@@ -129,10 +121,10 @@ int uart8250_init()
 		       (16 * uart8250_baudrate);
 	}
 
-	// /* Disable all interrupts */
-	// set_reg(UART_IER_OFFSET, 0x00);
-	// /* Enable DLAB */
-	// set_reg(UART_LCR_OFFSET, 0x80);
+	/* Disable all interrupts */
+	set_reg(UART_IER_OFFSET, 0x00);
+	/* Enable DLAB */
+	set_reg(UART_LCR_OFFSET, 0x80);
 
 	if (bdiv) {
 		/* Set divisor low byte */
@@ -141,18 +133,18 @@ int uart8250_init()
 		set_reg(UART_DLM_OFFSET, (bdiv >> 8) & 0xff);
 	}
 
-	// /* 8 bits, no parity, one stop bit */
-	// set_reg(UART_LCR_OFFSET, 0x03);
-	// /* Enable FIFO */
-	// set_reg(UART_FCR_OFFSET, 0x01);
-	// /* No modem control DTR RTS */
-	// set_reg(UART_MCR_OFFSET, 0x00);
-	// /* Clear line status */
-	// get_reg(UART_LSR_OFFSET);
-	// /* Read receive buffer */
-	// get_reg(UART_RBR_OFFSET);
-	// /* Set scratchpad */
-	// set_reg(UART_SCR_OFFSET, 0x00);
+	/* 8 bits, no parity, one stop bit */
+	set_reg(UART_LCR_OFFSET, 0x03);
+	/* Enable FIFO */
+	set_reg(UART_FCR_OFFSET, 0x01);
+	/* No modem control DTR RTS */
+	set_reg(UART_MCR_OFFSET, 0x00);
+	/* Clear line status */
+	get_reg(UART_LSR_OFFSET);
+	/* Read receive buffer */
+	get_reg(UART_RBR_OFFSET);
+	/* Set scratchpad */
+	set_reg(UART_SCR_OFFSET, 0x00);
 
 	return 0;
 }
